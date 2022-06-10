836989b8751e813dd9051d996716b980d7d57727
Indramohan Joshi <indramohanjoshi@gmail.com>
2022-06-11 00:22:43 +0200

    spring boot book microservice

---
+++ src/test/java/com/example/springbootdemo/springcloudfeignbookservice/SpringCloudFeignBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.springcloudfeignbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFeignBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
